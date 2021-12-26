8b11c61b72315b1fd330116a2839e03a99a694ae
SGrottel <webmaster@sgrottel.de>
2021-12-26 11:55:47 +0100

    Initial project setup for the simple server implementation in TypeScript with Node and Express

---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+//
+// Simple implementation of sg.statuslog
+// Available under MIT LICENSE
+//
+import express, { Request, Response, Application } from 'express';
+
+// express application
+const app: Application = express();
+
+// configuration
+
+const PORT = parseInt(process.env.PORT || '8000');
+
+function ensureApiRoot(str: string): string {
+	if (str.length === 0) return '/';
+	let s = str.toLocaleLowerCase().replace(/[^a-z0-9\/_\-.]/g, '');
+	if (s[0] !== '/') s = '/' + s;
+	if (s.slice(-1) !== '/') s = s + '/';
+	return s;
+}
+const API_ROOT = ensureApiRoot(process.env.API_ROOT || '/status/');
+
+if (API_ROOT !== '/') {
+	// install convenience route to get to the API_ROOT
+	app.get('/', (req: Request, res: Response): void => {
+		res.redirect(301, `http://localhost:${PORT}${API_ROOT}`);
+	});
+}
+
+// example route
+app.get(`${API_ROOT}`, (req: Request, res: Response): void => {
+	// TODO: Implement
+	res.send('sg.statuslog');
+});
+
+// start express application
+app.listen(PORT, ():void => {
+	console.log(`Running sg.statuslog at http://localhost:${PORT}${API_ROOT}`);
+});
